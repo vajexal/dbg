453e6aa04fd2c2fd36677766615f938ca0b30fd9
vajexal <vajexal@gmail.com>
2025-02-01 15:27:49 +0300

    print struct field

--- src/commands/print_var.rs
+++ src/commands/print_var.rs
@@ -1,14 +1,16 @@
 use anyhow::Result;
 
 use crate::debugger::Debugger;
-use crate::printer::Printer;
+use crate::printer::{InvalidPathError, Printer};
 
 pub fn print_var<R: gimli::Reader>(debugger: &Debugger<R>, name: Option<String>) -> Result<()> {
     let printer = Printer::new(debugger);
 
     match name {
         Some(name) => {
-            let var = match debugger.get_var(&name)? {
+            let path: Vec<&str> = name.split('.').collect();
+
+            let var = match debugger.get_var(path[0])? {
                 Some(var) => var,
                 None => {
                     println!("{} not found", name);
@@ -16,11 +18,16 @@ pub fn print_var<R: gimli::Reader>(debugger: &Debugger<R>, name: Option<String>)
                 }
             };
 
-            printer.print(&var)?;
+            if let Err(e) = printer.print(&var, &path[1..]) {
+                match e.downcast_ref::<InvalidPathError>() {
+                    Some(_) => println!("{}", e),
+                    None => return Err(e),
+                }
+            }
         }
         None => {
             for var in debugger.get_vars()?.iter() {
-                printer.print(var)?;
+                printer.print(var, &[])?;
             }
         }
     };
--- src/printer.rs
+++ src/printer.rs
@@ -2,9 +2,14 @@ use std::io::{self, Write};
 
 use anyhow::{bail, Result};
 use bytes::{Buf, Bytes};
+use thiserror::Error;
 
 use crate::debugger::{Debugger, WORD_SIZE};
-use crate::var::{Var, VarType};
+use crate::var::{Field, Var, VarType};
+
+#[derive(Error, Debug)]
+#[error("invalid path")]
+pub struct InvalidPathError;
 
 pub struct Printer<'a, R: gimli::Reader> {
     debugger: &'a Debugger<R>,
@@ -15,33 +20,55 @@ impl<'a, R: gimli::Reader> Printer<'a, R> {
         Self { debugger }
     }
 
-    pub fn print(&self, var: &Var<R>) -> Result<()> {
+    pub fn print(&self, var: &Var<R>, path: &[&str]) -> Result<()> {
         let mut lock = io::stdout().lock();
 
-        self.print_type(&mut lock, &var.typ)?;
+        self.print_type(&mut lock, &var.typ, path)?;
         write!(lock, " {} = ", var.name)?;
-        self.print_value(&mut lock, var)?;
+        self.print_value(&mut lock, var, path)?;
         write!(lock, "\n")?;
 
         Ok(())
     }
 
-    fn print_type(&self, f: &mut impl io::Write, var_type: &VarType) -> io::Result<()> {
+    fn print_type(&self, f: &mut impl io::Write, var_type: &VarType, path: &[&str]) -> Result<()> {
         match var_type {
-            VarType::Base { name, .. } => write!(f, "{}", name),
+            VarType::Base { name, .. } => {
+                if !path.is_empty() {
+                    bail!(InvalidPathError);
+                }
+
+                write!(f, "{}", name)?;
+            }
             VarType::Const(var_type) => {
                 write!(f, "const ")?;
-                self.print_type(f, var_type)
+                self.print_type(f, var_type, path)?;
             }
             VarType::Pointer(var_type) => {
-                self.print_type(f, var_type)?;
-                write!(f, "*")
+                if !path.is_empty() {
+                    // todo follow fields behind pointer
+                    bail!(InvalidPathError);
+                }
+
+                self.print_type(f, var_type, path)?;
+                write!(f, "*")?;
             }
-            VarType::Struct { name, .. } => write!(f, "{}", name),
+            VarType::Struct { name, fields, .. } => {
+                if path.is_empty() {
+                    write!(f, "{}", name)?;
+                } else {
+                    match fields.iter().find(|field| field.name == path[0]) {
+                        Some(field) => self.print_type(f, &field.typ, &path[1..])?,
+                        None => bail!(InvalidPathError),
+                    }
+                }
+            }
         }
+
+        Ok(())
     }
 
-    fn print_value(&self, f: &mut impl io::Write, var: &Var<R>) -> Result<()> {
+    fn print_value(&self, f: &mut impl io::Write, var: &Var<R>, path: &[&str]) -> Result<()> {
         let size = match var.typ.unwind() {
             VarType::Base { size, .. } => *size as usize,
             VarType::Const(_) => panic!("can't get const type size"),
@@ -50,12 +77,16 @@ impl<'a, R: gimli::Reader> Printer<'a, R> {
         };
 
         let buf = self.debugger.read_location(&var.location, size)?;
-        self.print_bytes(f, buf,&var.typ)
+        self.print_bytes(f, buf, &var.typ, path)
     }
 
-    fn print_bytes(&self, f: &mut impl io::Write, mut buf: Bytes, var_type: &VarType) -> Result<()> {
+    fn print_bytes(&self, f: &mut impl io::Write, mut buf: Bytes, var_type: &VarType, path: &[&str]) -> Result<()> {
         match var_type {
             VarType::Base { encoding, size, .. } => {
+                if !path.is_empty() {
+                    bail!(InvalidPathError);
+                }
+
                 match *encoding {
                     gimli::DW_ATE_boolean => write!(f, "{}", buf.get_u8() != 0)?,
                     gimli::DW_ATE_signed => match size {
@@ -75,8 +106,13 @@ impl<'a, R: gimli::Reader> Printer<'a, R> {
                     _ => bail!("unsupported encoding"),
                 };
             }
-            VarType::Const(var_type) => self.print_bytes(f, buf, var_type.as_ref())?,
+            VarType::Const(var_type) => self.print_bytes(f, buf, var_type.as_ref(), path)?,
             VarType::Pointer(var_type) => {
+                if !path.is_empty() {
+                    // todo follow fields behind pointer
+                    bail!(InvalidPathError);
+                }
+
                 let ptr = buf.get_u64_ne();
 
                 // check for char*
@@ -91,20 +127,33 @@ impl<'a, R: gimli::Reader> Printer<'a, R> {
                 write!(f, "{:#x}", ptr)?
             }
             VarType::Struct { fields, .. } => {
-                write!(f, "{{ ")?;
-
-                for (i, field) in fields.iter().enumerate() {
-                    if i != 0 {
-                        write!(f, ", ")?;
+                if path.is_empty() {
+                    self.print_struct_bytes(f, buf, fields)?;
+                } else {
+                    match fields.iter().find(|field| field.name == path[0]) {
+                        Some(field) => self.print_bytes(f, buf.slice((field.offset as usize)..), &field.typ, &path[1..])?,
+                        None => bail!(InvalidPathError),
                     }
-                    write!(f, "{} = ", field.name)?;
-
-                    self.print_bytes(f, buf.slice((field.offset as usize)..), &field.typ)?;
                 }
-
-                write!(f, " }}")?;
-            },
+            }
         };
+
+        Ok(())
+    }
+
+    fn print_struct_bytes(&self, f: &mut impl io::Write, buf: Bytes, fields: &Vec<Field>) -> Result<()> {
+        write!(f, "{{ ")?;
+
+        for (i, field) in fields.iter().enumerate() {
+            if i != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{} = ", field.name)?;
+
+            self.print_bytes(f, buf.slice((field.offset as usize)..), &field.typ, &[])?;
+        }
+
+        write!(f, " }}")?;
 
         Ok(())
     }
