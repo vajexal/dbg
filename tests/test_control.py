68ce03f6c0e3ce7c0d89cf6596b322a69632a3df
vajexal <vajexal@gmail.com>
2025-03-15 13:42:52 +0300

    test control

---
+++ tests/test_control.py
@@ -0,0 +1,56 @@
+from tests import Step
+
+
+def test_run(debugger):
+    debugger(
+        code="""#include <stdio.h>
+
+int main()
+{
+    printf("hello world\\n");
+    return 0;
+}
+""",
+        steps=[
+            Step("b 5", "breakpoint set"),
+            Step("r"),
+            Step("r", "invalid command"),
+            Step("c", "hello world"),
+            Step("r", "invalid command"),
+            Step("q"),
+        ]
+    )
+
+
+def test_quit_started_program(debugger):
+    debugger(
+        code="""#include <stdio.h>
+
+int main()
+{
+    printf("hello world\\n");
+    return 0;
+}
+""",
+        steps=[
+            Step("q"),
+        ]
+    )
+
+
+def test_quit_running_program(debugger):
+    debugger(
+        code="""#include <stdio.h>
+
+int main()
+{
+    printf("hello world\\n");
+    return 0;
+}
+""",
+        steps=[
+            Step("b 5", "breakpoint set"),
+            Step("r"),
+            Step("q"),
+        ]
+    )
